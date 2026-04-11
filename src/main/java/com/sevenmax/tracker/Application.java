4861ef7abd2ffede188a68a933491a1fba3dd65e
rahavmagen <rahavm@gmail.com>
2026-04-11 12:07:11 +0300

    Fix timezone: use Asia/Jerusalem for all LocalDate.now() calls

--- src/main/java/com/sevenmax/tracker/Application.java
+++ src/main/java/com/sevenmax/tracker/Application.java
@@ -3,10 +3,13 @@ package com.sevenmax.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Jerusalem"));
 		SpringApplication.run(Application.class, args);
 	}
 
